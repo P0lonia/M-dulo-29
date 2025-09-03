e901bd1601723d2d2672c2717743c63623c8e2bc
P0lonia <gazunano@gmail.com>
2025-09-03 20:24:30 -0300

    Estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/P0lonia.png" alt="" />
+
+export default Avatar
--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/P0lonia.png" />
-    <Titulo fontSize={20}>P0lonia</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gustavo Ferraz</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        P0lonia
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor-FullStack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
